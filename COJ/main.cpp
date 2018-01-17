9c6b61848fdbfc597f649bc071da19a7b7819844
Erick González <A01039859@itesm.mx>
2018-01-17 10:18:50 -0600

    Initial Commit

---
+++ COJ/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COJ
+//
+//  Created by Erick González on 1/17/18.
+//  Copyright © 2018 Erick González. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
